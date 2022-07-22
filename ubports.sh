09e8af94f6f3ee4106481d8a505fa2130c611fcc
dopaemon <polarisdp@gmail.com>
2022-07-22 15:03:02 +0700

    Initial
    Signed-off-by: dopaemon <polarisdp@gmail.com>

---
+++ ubports.sh
@@ -0,0 +1,30 @@
+# data/linux Touch Port
+# Base Flashable With Erfan . Edit By Nobi Nobita
+
+OUTFD=/proc/self/fd/$1;
+
+# ui_print <text>
+ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
+
+## data/linux Touch Install For Sweet
+
+# Remove Old File
+rm -rf /data/ubuntu.img;
+
+# Copy And Patch New File
+mv -f /data/ubports/data/ubuntu.img /data/;
+
+# Create Folder Mount
+mkdir -p /data/linux/ubuntu;
+
+# Mount *.img to Folder Mount
+mount /data/ubuntu.img /data/linux/ubuntu;
+
+# Umount All *.img
+umount /data/linux/ubuntu;
+
+# Remove Install file
+rm -rf /data/ubports;
+rm -rf /data/linux;
+
+## Install Done ##
